b4476cf65f2b6511d0958b4cfa000b1d91393a1c
Unknown <bastien.chies@epsi.fr>
2018-04-10 15:02:08 +0200

    Fichiers liés à l'authentification
    Ajout des fichiers liés à l'authentification (dont les entités) et modification du fichier de routes.

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,18 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Enseignant;
+use App\Entity\Etudiant;
+use App\Entity\ResponsablePedago;
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,10 +22,92 @@ class LoginController extends Controller
     /**
      * @Route("/login", name="login")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('login/login.html.twig', [
-            'controller_name' => 'LoginController',
-        ]);
+
+
+
+        $form = $this->createFormBuilder()
+            ->add('mail', EmailType::class, [
+                'label' => 'Adresse Email',
+                'attr' => ['class' => 'form-control','name' => 'email','placeholder' => 'Adresse mail'],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'attr' => ['class' => 'form-control','name' => 'password','placeholder' => 'Mot de passe'],
+            ])
+            ->add('statut', ChoiceType::class, [
+                'label' => 'Statut:',
+                'attr' => ['class' => 'form-control','name' => 'statut'],
+                'choices' => array(
+                    'Etudiant' => 0,
+                    'Intervenant' => 1,
+                    'Responsable pédagogique' => 2
+                )
+            ])
+            ->add('connexion', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-block bg-pink waves-effect', 'type' => 'submit', 'value' => 'Connexion'],
+            ])
+
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $user = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            //$entityManager = $this->getDoctrine()->getManager();
+            //$entityManager->
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+            //Verification des données renvoyées par le formulaire.
+            //var_dump($user);
+
+            switch ($user['statut'])
+            {
+                case 0:
+                    $repository = $this->getDoctrine()->getRepository(Etudiant::class);
+                    break;
+
+                case 1:
+                    $repository = $this->getDoctrine()->getRepository(Enseignant::class);
+                    break;
+
+                case 2:
+                    $repository = $this->getDoctrine()->getRepository(ResponsablePedago::class);
+                    break;
+
+                default:
+                    $repository = null;
+                    break;
+            }
+            $utilisateur = $repository->findOneBy([
+                'mail' => $user['mail'],
+                'motdepasse' => $user['password'],
+            ]);
+            if($utilisateur != null) {
+                return $this->redirectToRoute('accueil', array());
+            }
+            else
+            {
+                return $this->render('login/login.html.twig',array(
+                    'form' => $form->createView(),
+                    'errorMessage' => 'Identifiants invalides.'
+                ));
+            }
+        }
+
+        return $this->render('login/login.html.twig',array(
+            'form' => $form->createView(),
+            'errorMessage' => ''
+        ));
+    }
+
+    public function checkLogin(string $userMail, string $userPass)
+    {
+
     }
 }
---
+++ src/Entity/Etudiant.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EtudiantRepository")
+ * @ORM\Table(name="eleve")
+ */
+class Etudiant
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $motdepasse;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setMail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMotdepasse(): ?string
+    {
+        return $this->motdepasse;
+    }
+
+    public function setMotdepasse(string $motdepasse): self
+    {
+        $this->motdepasse = $motdepasse;
+
+        return $this;
+    }
+}
