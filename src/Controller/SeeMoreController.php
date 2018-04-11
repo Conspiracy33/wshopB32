4d45f8d46741333e8a57a87e717482167d978de9
blabla <blabla@test.fr>
2018-04-11 16:03:09 +0200

    Last Commit to Bastien

---
+++ src/Controller/SeeMoreController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SeeMoreController extends Controller
+{
+    /**
+     * @Route("/see/more", name="see_more")
+     */
+    public function index()
+    {
+        return $this->render('see_more/index.html.twig', [
+            'controller_name' => 'SeeMoreController',
+        ]);
+    }
+}
